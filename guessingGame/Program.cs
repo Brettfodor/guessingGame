150e9d1d5c45cacb8e3d7239fcc5e3e193270c0b
Brett Fodor <bmattmusic@gmail.com>
2019-09-05 13:55:06 -0400

    win condition

--- guessingGame/Program.cs
+++ guessingGame/Program.cs
@@ -6,7 +6,25 @@ namespace guessingGame
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+
+            Console.WriteLine("Please enter a number.");
+
+            string number = Console.ReadLine();
+
+            bool seven = number.Equals("7");
+
+            switch (number)
+            {
+                case "7":
+                    Console.WriteLine("You have won the game");
+                    break;
+
+                default:
+                    Console.WriteLine("You Lose");
+                    break;
+
+            }
         }
     }
 }
