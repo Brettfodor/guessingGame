ea1e7808e5055247903428cb37ccdd991dbdc50c
Brett Fodor <bmattmusic@gmail.com>
2019-09-05 13:59:14 -0400

    instructions for 0 value

--- guessingGame/Program.cs
+++ guessingGame/Program.cs
@@ -13,11 +13,16 @@ namespace guessingGame
             string number = Console.ReadLine();
 
             bool seven = number.Equals("7");
+            bool zero = number.Equals("0");
 
             switch (number)
             {
                 case "7":
                     Console.WriteLine("You have won the game");
+                    break;
+
+                case "0":
+                    Console.WriteLine("Your number is a bit low");
                     break;
 
                 default:
